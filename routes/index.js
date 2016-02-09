78b79507206bee96e08bd2249306769da6c7feb6
Miguel Santamaria <msantamaria@hugeinc.com>
2016-02-09 09:08:45 -0500

    Changes and Heroku integration.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Digital Pensum' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Digital Pensum' });
+// });
+
+router.get('/', function(req, res) {
+  console.log(req.query);
 });
 
 module.exports = router;
