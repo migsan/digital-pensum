ff409d7a307f1bd7da9ca739398727582e2c6764
Miguel Santamaria <msantamaria@hugeinc.com>
2016-02-09 09:11:15 -0500

    Added index file to render on Get.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Digital Pensum' });
-// });
-
-router.get('/', function(req, res) {
-  console.log(req.query);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Digital Pensum' });
 });
 
 module.exports = router;
